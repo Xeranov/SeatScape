af70e041685eb4b997da0bf54e94013285ed1b74
Hanifxys <mhmdhanif784@gmail.com>
2023-10-16 15:20:34 +0700

    Memperbaiki navigasi buat burger, deskripsi produk, nama, footer. Update link WhatsApp. Mengubah ukuran font untuk responsif.

--- index.js
+++ index.js
@@ -6,16 +6,22 @@ function scrollToTop() {
 window.onscroll = function() {
     var scrollHeight, totalHeight, scrollY;
     
-    scrollHeight = document.body.scrollHeight; // Height of the entire document
-    totalHeight = window.scrollY + window.innerHeight; // Y position from top + browser height
+    scrollHeight = document.body.scrollHeight;
+    totalHeight = window.scrollY + window.innerHeight;
     scrollY = window.scrollY;
 
-    // Display button if user has scrolled past 500px from the top of the page
     if (scrollY > 500 || totalHeight >= scrollHeight) {
         document.getElementById("scrollToTopButton").style.display = "block";
     } else {
         document.getElementById("scrollToTopButton").style.display = "none";
     }
-
-    
 };
+
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.getElementById('mobile-menu');
+    const navList = document.querySelector('.nav-list');
+
+    menuToggle.addEventListener('click', function() {
+        navList.classList.toggle('active');
+    });
+});
